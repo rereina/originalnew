9022e47bbcce82085035b12979b2ec2c5f89112a
佐藤　玲奈 <rerepi5834coo@gmail.com>
2015-06-28 08:40:35 +0900

    pre

--- original application/ViewController2.swift
+++ original application/ViewController2.swift
@@ -7,8 +7,86 @@
 //
 
 import UIKit
+import UIKit
 
-class ViewController2: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController2: UIViewController,UIPickerViewDelegate,UIToolbarDelegate {
+    
+    var salarymanArr: NSArray = ["リビング", "自分の部屋", "洗面所", "トイレ"]
+    var myToolBar: UIToolbar!
+    var myTextField: UITextField!
+    var myPickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //PickerView作成
+        myPickerView = UIPickerView()
+        myPickerView.showsSelectionIndicator = true
+        myPickerView.delegate = self
+        
+        //TextField作成
+        myTextField = UITextField(frame: CGRectMake(150, 300, 320, 50))
+        myTextField.placeholder = salarymanArr[0] as? String;
+        self.view.addSubview(myTextField)
+        
+        //ToolBar作成。ニョキ担当
+        myToolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
+        myToolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        myToolBar.backgroundColor = UIColor.blackColor()
+        myToolBar.barStyle = UIBarStyle.Black
+        myToolBar.tintColor = UIColor.whiteColor()
+        
+        //ToolBarを閉じるボタンを追加
+        let myToolBarButton = UIBarButtonItem(title: "Close", style: .Bordered, target: self, action: "onClick:")
+        myToolBarButton.tag = 1
+        myToolBar.items = [myToolBarButton]
+        
+        //TextFieldをpickerViewとToolVerに関連づけ
+        myTextField.inputView = myPickerView
+        myTextField.inputAccessoryView = myToolBar
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int {
+        return salarymanArr.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return salarymanArr[row] as? String;
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myTextField.text = salarymanArr[row] as? String;
+    }
+    
+    //閉じる
+    func onClick(sender: UIBarButtonItem) {
+        myTextField.resignFirstResponder()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
+
+/*class ViewController2: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     var places = ["リビング", "自分の部屋", "洗面所", "トイレ"]
     //数値の範囲（1...10という記法）を配列にする
     //var dataArray:[Int] = ([Int])(0...24)
@@ -89,6 +167,6 @@ class ViewController2: UIViewController, UIPickerViewDataSource, UIPickerViewDel
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    */*/
 
 
--- original application/ViewController3.swift
+++ original application/ViewController3.swift
@@ -9,11 +9,161 @@
 import UIKit
 
 
-class ViewController3: UIViewController,UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController3: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate{
+    
+    
+    var myUIPicker: UIPickerView = UIPickerView()
+    
+    var salarymanArr: NSArray =
+    
+    ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"]
+    
+    var yakushokuArr: NSArray = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"]
+    
+    var kinmuchiArr: NSArray = [""]
     
     
     
-    //private var myTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        
+        myUIPicker.frame = CGRectMake(0,80,self.view.bounds.width, 180.0)
+        
+        myUIPicker.delegate = self
+        
+        myUIPicker.dataSource = self
+        
+        self.view.addSubview(myUIPicker)
+        
+    }
+    
+    
+    
+    //表示列
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        return 3
+        
+    }
+    
+    
+    
+    //表示個数
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        
+        
+        if (component == 0){
+            
+            return salarymanArr.count
+            
+        }else if (component == 1){
+            
+            return yakushokuArr.count
+            
+        }else if (component == 2){
+            
+            return kinmuchiArr.count
+            
+        }
+        
+        return 0;
+        
+    }
+    
+    
+    
+    //表示内容
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
+        
+        
+        
+        if (component == 0){
+            
+            return salarymanArr[row] as! String
+            
+        }else if (component == 1){
+            
+            return yakushokuArr[row] as! String
+            
+        }else if (component == 2){
+            
+            return kinmuchiArr[row] as! String
+            
+        }
+        
+        return "";
+        
+    }
+    
+    
+    
+    //選択時
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        
+        
+        if (component == 0){
+            
+            println("列: \(row)")
+            
+            println("値: \(salarymanArr[row])")
+            
+        }else if (component == 1){
+            
+            println("列: \(row)")
+            
+            println("値: \(yakushokuArr[row])")
+            
+        }else if (component == 2){
+            
+            println("列: \(row)")
+            
+            println("値: \(kinmuchiArr[row])")
+            
+        }
+        
+        
+        
+    }
+    
+    @IBAction func perfect( sender:AnyObject){
+        // データ保存処理
+        
+        // NSUserDefaultsインスタンスの生成
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        // キー: "saveText" , 値: "" を格納。（idは任意）
+        //userDefaults.setObject(textField.text, forKey: "saveText")
+        
+    }
+    
+    func check (sender:AnyObject){
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+    
+}
+
+
+  /*  //private var myTextField: UITextField!
     @IBOutlet weak var perfect: UIButton!
     @IBOutlet weak var check: UIButton!
 
@@ -81,9 +231,9 @@ class ViewController3: UIViewController,UITextFieldDelegate, UIPickerViewDataSou
         override func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
-        }
+        }*/
     
-    @IBAction func perfect( sender:AnyObject){
+   /* @IBAction func perfect( sender:AnyObject){
         // データ保存処理
         
         // NSUserDefaultsインスタンスの生成
@@ -94,9 +244,9 @@ class ViewController3: UIViewController,UITextFieldDelegate, UIPickerViewDataSou
         
     }
     
-    @IBAction func check (sender:AnyObject){
+     func check (sender:AnyObject){
     }
-}
+*/
 /*/*
 UITextFieldが編集された直後に呼ばれるデリゲートメソッド.
 */
